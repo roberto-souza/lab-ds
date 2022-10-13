e405f42386ddeee73f6025f06515eb4c81e7406e
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2022-10-13 16:20:46 -0300

    Criação do componente de TextInput com storybook

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Envelope } from 'phosphor-react'
+
+import { ITextInputRootProps, TextInput } from './TextInput'
+
+export default {
+  title: 'Components/TextInput',
+  component: TextInput.Root,
+  args: {
+    children: [
+      <TextInput.Icon>
+        <Envelope />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder="Type your e-mail address" />
+    ]
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+} as Meta<ITextInputRootProps>
+
+export const Default: StoryObj<ITextInputRootProps> = {}
+
+export const WithoutIcon: StoryObj<ITextInputRootProps> = {
+  args: {
+    children: (
+      <TextInput.Input placeholder="Type your e-mail address" />
+    )
+  }
+}
