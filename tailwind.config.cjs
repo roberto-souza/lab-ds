7476443bcf349e0424ad310bc06e65f1929c29eb
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2022-10-13 15:15:30 -0300

    Criação do componente de Button com storybook

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,13 +11,15 @@ module.exports = {
       '2xl': 32,
     },
     colors: {
+      'black': '#000',
+      'cyan-500': '#81d8f7',
+      'cyan-300': '#9be1fb',
       'gray-900': '#121214',
       'gray-800': '#202024',
       'gray-400': '#7c7c8a',
       'gray-200': '#c4c4cc',
       'gray-100': '#e1e1e6',
-      'cyan-500': '#81d8f7',
-      'cyan-300': '#9be1fb',
+      'white': '#fff',
     },
     extend: {
       fontFamily: {
